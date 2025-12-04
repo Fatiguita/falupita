7d4245017393e88c9fc0e95de8d7c109c38a9924
Fatiguita <129623577+Fatiguita@users.noreply.github.com>
2025-12-04 15:12:32 -0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// The styling was broken because this import was missing:
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
